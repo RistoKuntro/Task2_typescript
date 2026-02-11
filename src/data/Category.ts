a0d9c106d4d09d2d0e998f9fba8fc0e7fb24e153
RistoKuntro <kuntro.risto@gmail.com>
2026-02-11 14:30:28 +0200

    Basic files complete.

---
+++ src/data/Category.ts
@@ -0,0 +1,6 @@
+export enum Category {
+    Electronics,
+    Books,
+    Accessories,
+    Clothing
+}
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import type { Supplier } from "./data/Supplier";
+
+let categories: string[] = ["Electronics", "Books", "Accessories", "Clothing"];
+
+const suppliers: Supplier = {
+    id: 1,
+    name: "Tech Supplies Inc.",
+}
